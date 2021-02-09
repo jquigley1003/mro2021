3aba878b3e7f3da79ae8213bd37432bc781d97ef
Jeff Quigley <jeff.quigley@wavinghi.com>
2021-02-08 21:08:02 -0500

    update Angular from 10 to 11

--- src/app/credentials/credentials.page.spec.ts
+++ src/app/credentials/credentials.page.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { CredentialsPage } from './credentials.page';
@@ -7,7 +7,7 @@ describe('CredentialsPage', () => {
   let component: CredentialsPage;
   let fixture: ComponentFixture<CredentialsPage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CredentialsPage ],
       imports: [IonicModule.forRoot()]
--- src/app/shared/components/my-menubar/my-menubar.component.spec.ts
+++ src/app/shared/components/my-menubar/my-menubar.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { MyMenubarComponent } from './my-menubar.component';
@@ -7,7 +7,7 @@ describe('MyMenubarComponent', () => {
   let component: MyMenubarComponent;
   let fixture: ComponentFixture<MyMenubarComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MyMenubarComponent ],
       imports: [IonicModule.forRoot()]
